6b233174c22bc19756d238229723ec3df310741f
Dipen <dipenlama9937@gmail.com>
2024-08-17 11:32:25 +0545

    Log out btn added

--- src/Bagaicha/Seller.java
+++ src/Bagaicha/Seller.java
@@ -100,7 +100,7 @@ String sql = "select * from Employee";
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         employeetable = new javax.swing.JTable();
-        billingbtn1 = new javax.swing.JButton();
+        logoutbtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -265,19 +265,19 @@ String sql = "select * from Employee";
                 .addGap(0, 17, Short.MAX_VALUE))
         );
 
-        billingbtn1.setBackground(new java.awt.Color(255, 153, 0));
-        billingbtn1.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
-        billingbtn1.setForeground(new java.awt.Color(255, 255, 255));
-        billingbtn1.setText("GO BACK");
-        billingbtn1.setFocusable(false);
-        billingbtn1.addMouseListener(new java.awt.event.MouseAdapter() {
+        logoutbtn.setBackground(new java.awt.Color(255, 153, 0));
+        logoutbtn.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        logoutbtn.setForeground(new java.awt.Color(255, 255, 255));
+        logoutbtn.setText("Log Out");
+        logoutbtn.setFocusable(false);
+        logoutbtn.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                billingbtn1MouseClicked(evt);
+                logoutbtnMouseClicked(evt);
             }
         });
-        billingbtn1.addActionListener(new java.awt.event.ActionListener() {
+        logoutbtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                billingbtn1ActionPerformed(evt);
+                logoutbtnActionPerformed(evt);
             }
         });
 
@@ -287,7 +287,7 @@ String sql = "select * from Employee";
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(billingbtn1)
+                .addComponent(logoutbtn)
                 .addGap(105, 105, 105)
                 .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 245, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -341,13 +341,13 @@ String sql = "select * from Employee";
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(20, 20, 20)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(billingbtn1)
-                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(logoutbtn)
+                            .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(employeeidField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -590,13 +590,17 @@ String sql = "select * from Employee";
         // TODO add your handling code here:
     }//GEN-LAST:event_nameFieldActionPerformed
 
-    private void billingbtn1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_billingbtn1MouseClicked
+    private void logoutbtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_logoutbtnMouseClicked
         // TODO add your handling code here:
-    }//GEN-LAST:event_billingbtn1MouseClicked
+    }//GEN-LAST:event_logoutbtnMouseClicked
 
-    private void billingbtn1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_billingbtn1ActionPerformed
+    private void logoutbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_logoutbtnActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_billingbtn1ActionPerformed
+        login logout=new login();
+        this.dispose();
+        logout.setVisible(true);
+        
+    }//GEN-LAST:event_logoutbtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -635,7 +639,6 @@ String sql = "select * from Employee";
             }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton addbtn;
-    private javax.swing.JButton billingbtn1;
     private javax.swing.JButton clearbtn;
     private javax.swing.JButton deletebtn;
     private javax.swing.JButton editbtn;
@@ -652,6 +655,7 @@ String sql = "select * from Employee";
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton logoutbtn;
     private javax.swing.JTextField nameField;
     private javax.swing.JTextField passwordField;
     // End of variables declaration//GEN-END:variables
